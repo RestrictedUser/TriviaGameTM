d96758e68514b22c8106095cbabc2ab1f3e35b4a
RestrictedTM <tylermiddleton60@gmail.com>
2018-09-21 16:43:51 -0700

    Final Final Draft

--- Assets/Javascript/game.js
+++ Assets/Javascript/game.js
@@ -31,9 +31,7 @@ function move() {
         counter.innerHTML = "0:" + (seconds < 10 ? "0" : "") + String(seconds);
         if( seconds > 0 ) {
             setTimeout(tick, 1000);
-        } else {
-            alert("Time's up click ok or press Enter to retry.");
-        }
+        } 
     }
     tick();
 }
